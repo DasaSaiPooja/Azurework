2df8d6f8f9ef6b3f7aef0f89f7523601afb94574
DasaSaiPooja <157349181+DasaSaiPooja@users.noreply.github.com>
2024-04-18 13:55:21 +0530

    Add files via upload

---
+++ webapp/Startup.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace webapp
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            // Log requests
+            app.Use(async (context, next) =>
+            {
+                string fullUrl = $"{context.Request.Scheme}://{context.Request.Host}{context.Request.Path}{context.Request.QueryString}";
+                string logMessage = $"{DateTime.Now} - Requested Path:{fullUrl}\n";
+
+                string logFilePath = Path.Combine(env.ContentRootPath, "log.txt");
+                await File.AppendAllTextAsync(logFilePath, logMessage, Encoding.UTF8);
+
+                await next.Invoke();
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
